8f1df4c1da940d7f45baf4c1bfa89bc070dcc727
mahmoud abubaker <mahmmuod.b2010@gmail.com>
2022-12-08 19:08:20 +0200

    update logo,style file and js code

--- Login.js
+++ Login.js
@@ -1,16 +1,19 @@
-const loginForm = Login.html.getElementById("login form");
-const loginButton = Login.html.getElementById("Login_button");
-const loginErrorMsg = Login.html.getElementById("login-error-msg");
+const employeeNum = document.getElementById("employee_num");
+const loginButton = document.getElementById("Login_button");
+const loginErrorMsg = document.getElementById("login-error-msg");
+const badge_num = document.getElementById("badge_num");
 
 loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
+  e.preventDefault();
+  const username = employeeNum.value;
+  const password = badge_num.value;
 
-    if (username === "1001" && password === "1002") {
-        alert("You have successfully logged in.");
-        location.reload();
-    } else {
-        loginErrorMsg.style.opacity = 1;
-    }
-})
+  if (username === "1001" && password === "1002") {
+    alert("You have successfully logged in.");
+    loginErrorMsg.style.display = "none";
+
+    location.reload();
+  } else {
+    loginErrorMsg.style.display = "block";
+  }
+});
